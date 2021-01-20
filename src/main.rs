552afa7d7b2247ebb6a23be9b458573e3c87e967
John Ward <john@johnward.net>
2021-01-20 22:55:27 +0000

    Added Logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use actix_web::middleware::Logger;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world");
+    HttpResponse::Ok().body("Hello world!")
 }
 
 #[post("/echo")]
@@ -11,15 +13,19 @@ async fn echo(req_body: String) -> impl Responder {
 }
 
 async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!");
+    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
